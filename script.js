642a38859458e605d84b6b0f669f9244aed721fe
Afzal Ahmad <securedeveloper@gmail.com>
2020-02-11 20:59:56 +0100

    update script to handle query parameters

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-var appHolder;
+let appHolder;
 
-var barSpeed = 10; //in milliseconds
-var boxSize = 25;
+const urlParams = new URLSearchParams(window.location.search);
+const barSpeed = urlParams.has('speed') ? Number(urlParams.get('speed')) : 10; //in milliseconds
+const boxSize = urlParams.has('size') ? Number(urlParams.get('size')) : 25;
+const randomFlip = urlParams.has('random') ? Boolean(JSON.parse(urlParams.get('random'))) : true;
+const colorMode = urlParams.has('colormode') ? Boolean(JSON.parse(urlParams.get('colormode'))) : true;
+const borderSize = Math.round((boxSize/10)+1)
 
-function startBarDrawing() {
+console.log({
+    barSpeed,
+    boxSize,
+    randomFlip,
+    colorMode
+})
+
+const startBarDrawing = () => {
     appHolder = document.getElementById('app');
 
     const {
@@ -17,102 +28,87 @@ function startBarDrawing() {
     const rows = [];
     const cols = [];
 
+    const createNewBarElement = (type, position, counter) => {
+        let barElement = document.createElement('div');
+        barElement.style.position = 'fixed';
+        barElement.style.left = `${position}px`;
+        barElement.style.top = `${position}px`;
+
+        barElement.className = `${type} ${type}${counter}`;
+        barElement.id = `row${rowCounter}`;
+        barElement.setAttribute('data-type', position);
+        barElement.setAttribute('data-index', counter);
+        barElement.setAttribute('data-position', position);
+        barElement.setAttribute('data-incrementStyle', '1');
+
+        return barElement;
+    }
+
 
     let rowCounter = 0;
     
     for (let rowPosition = gridBoxSizeY; rowPosition < availHeight; rowPosition += gridBoxSizeY) {
-        let barElement = document.createElement('div');
-        barElement.style.position = 'fixed';
-        barElement.style.left = `${rowPosition}px`;
-        barElement.style.top = `${rowPosition}px`;
-
-        barElement.className = `row row${rowCounter++}`;
-        barElement.id = `row${rowCounter}`;
-        barElement.setAttribute('data-type', 'row');
-        barElement.setAttribute('data-index', rowCounter);
-        barElement.setAttribute('data-position', '0');
-        barElement.setAttribute('data-incrementStyle', '1');
-        rows.push(barElement);
-        appHolder.appendChild(barElement);
+        const rowBarElement = createNewBarElement('row', rowPosition, rowCounter++);
+        
+        rows.push(rowBarElement);
+        appHolder.appendChild(rowBarElement);
     }
 
     let colCounter = 0;
     for (let colPosition = gridBoxSizeX; colPosition < availWidth; colPosition += gridBoxSizeX) {
-        let barElement = document.createElement('div');
-        barElement.style.position = 'fixed';
-        barElement.style.left = `${colPosition}px`;
-        barElement.style.top = `${colPosition}px`;
-
-        barElement.className = `col col${colCounter++}`;
-        barElement.id = `col${rowCounter}`;
-        barElement.setAttribute('data-type', 'col');
-        barElement.setAttribute('data-index', colCounter);
-        barElement.setAttribute('data-position', '0');
-        barElement.setAttribute('data-incrementStyle', '1');
-        cols.push(barElement);
-        appHolder.appendChild(barElement);
+        const colBarElement = createNewBarElement('col', colPosition, colCounter++);
+        
+        cols.push(colBarElement);
+        appHolder.appendChild(colBarElement);
     }
 
     let barAnimator;
 
     const randomColor = () => {
+        if (!colorMode) return '#000';
         return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
     };
 
-    const isRandomTurn = () => Number(Math.random()).toFixed(3) === "0.683"
+    const isRandomTurn = () => randomFlip && Number(Math.random()).toFixed(3) === "0.683";
+
+    const nextPosition = (elem, maxVal) => {
+        let position = parseInt(elem.dataset.position);
+
+        if (elem.dataset.incrementstyle == '1') {
+            if (position + 1 >= maxVal || isRandomTurn()) {
+                elem.setAttribute('data-incrementStyle', '2');
+                elem.style.border = `${borderSize}px solid ${randomColor()}`;
+                position--;
+            } else {
+                position++;
+            }
+        } else {
+            if (position - 1 <= 0 || isRandomTurn()) {
+                elem.setAttribute('data-incrementStyle', '1');
+                elem.style.border = `${borderSize}px solid ${randomColor()}`;
+                position++;
+            } else {
+                position--;
+            }
+        }
+
+        elem.setAttribute('data-position', `${position}`);
+
+        return position;
+    }
 
     const startAnimation = () => {
         barAnimator = setInterval(() => {
             for (let r = 0; r < rowCounter; r++) {
                 const elem = rows[r];
-                let position = parseInt(elem.dataset.position);
-
-                if (elem.dataset.incrementstyle == '1') {
-                    if (position + 1 >= availWidth || isRandomTurn()) {
-                        elem.setAttribute('data-incrementStyle', '2');
-                        elem.style.border = `1px solid ${randomColor()}`;
-                        position--;
-                    } else {
-                        position++;
-                    }
-                } else {
-                    if (position - 1 <= 0 || isRandomTurn()) {
-                        elem.setAttribute('data-incrementStyle', '1');
-                        elem.style.border = `1px solid ${randomColor()}`;
-                        position++;
-                    } else {
-                        position--;
-                    }
-                }
-
-                elem.setAttribute('data-position', `${position}`);
+                const position = nextPosition(elem, availWidth);
                 // elem.style.opacity = `${Math.random()}`;
                 elem.style.left = `${position}px`;
             }
 
             for (let c = 0; c < colCounter; c++) {
                 const elem = cols[c];
-                let position = parseInt(elem.dataset.position);
-
-                if (elem.dataset.incrementstyle == '1') {
-                    if (position + 1 >= availHeight || isRandomTurn()) {
-                        elem.setAttribute('data-incrementStyle', '2');
-                        elem.style.border = `1px solid ${randomColor()}`;
-                        position--;
-                    } else {
-                        position++;
-                    }
-                } else {
-                    if (position - 1 <= 0 || isRandomTurn()) {
-                        elem.setAttribute('data-incrementStyle', '1');
-                        elem.style.border = `1px solid ${randomColor()}`;
-                        position++;
-                    } else {
-                        position--;
-                    }
-                }
-
-                elem.setAttribute('data-position', `${position}`);
+                const position = nextPosition(elem, availHeight);
                 // elem.style.opacity = `${Math.random()}`;
                 elem.style.top = `${position}px`;
             }
